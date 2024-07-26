1eae09f50755621ca99260f2eb1be26fbdc8f296
Muhammad Hadi Naeem <hadinaeem365@gmail.com>
2024-07-26 19:21:32 +0500

    Create EnemyAnimations.cs

---
+++ Assets/Scripts/EnemyAnimations.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAnimations : MonoBehaviour
+{
+    Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (EnemyMovement.isWalking == true)
+        {
+            animator.SetBool("isWalking", true);
+        }
+        if (EnemyMovement.isWalking == false)
+        {
+            animator.SetBool("isWalking", false);
+        }
+    }
+}
