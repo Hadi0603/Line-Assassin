579624f4ee61c071b90014d3901e82fcd7f31e23
Muhammad Hadi Naeem <hadinaeem365@gmail.com>
2024-07-27 18:34:21 +0500

    Create LevelLoader.cs

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelLoader : MonoBehaviour
+{
+    public GameObject loadingScreen;
+    public Slider slider;
+    public Text progressText;
+    public int sceneIndex;
+    // Start is called before the first frame update
+    void Start()
+    {
+        loadLevel(sceneIndex);
+    }
+    public void loadLevel(int sceneIndex)
+    {
+        StartCoroutine(LoadAsynchronously(sceneIndex));
+    }
+    IEnumerator LoadAsynchronously(int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+        loadingScreen.SetActive(true);
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+            slider.value = progress;
+            progressText.text = progress * 100 + "%";
+            yield return null;
+        }
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
