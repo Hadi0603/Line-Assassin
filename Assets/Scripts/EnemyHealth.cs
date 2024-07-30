d7a49131cc83ce06dae806ab67576473ea19c693
Muhammad Hadi Naeem <hadinaeem365@gmail.com>
2024-07-30 19:50:31 +0500

    Update EnemyHealth.cs

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -3,11 +3,17 @@ using UnityEngine;
 public class EnemyHealth : MonoBehaviour
 {
     public int health = 100;
+    private EnemyMovement enemy;
     [SerializeField] GameObject attackEffect;
+    private void Start()
+    {
+        enemy = GetComponent<EnemyMovement>();
+    }
 
     public void TakeDamage(int damage)
     {
         health -= damage;
+        enemy.NotifyNearbyEnemies();
         GameObject effectAtt = (GameObject)Instantiate(attackEffect, transform.position, transform.rotation);
         Destroy(effectAtt, 1f);
         if (health <= 0)
@@ -18,6 +24,7 @@ public class EnemyHealth : MonoBehaviour
 
     private void Die()
     {
+        LevelStats.enemiesKilled++;
         Destroy(gameObject);
     }
 }
