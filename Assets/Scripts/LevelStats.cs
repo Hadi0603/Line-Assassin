b6db2336a1dfd1d7a1d94adde55e5bb24c595ec1
Muhammad Hadi Naeem <hadinaeem365@gmail.com>
2024-07-30 21:16:05 +0500

    Update LevelStats.cs

--- Assets/Scripts/LevelStats.cs
+++ Assets/Scripts/LevelStats.cs
@@ -10,25 +10,33 @@ public class LevelStats : MonoBehaviour
     [SerializeField] Text healthCount;
     [SerializeField] Text starCount;
     [SerializeField] int totalStar;
+    [SerializeField] int totalDiamond;
+    [SerializeField] Text diamondCount;
     [SerializeField] GameObject levelCompleteUI;
-    public static int enemiesKilled = 0;
-    public static int starCollected = 0;
+    public static int enemiesKilled;
+    public static int starCollected;
+    public static int diamondCollected;
     private void Start()
     {
         enemiesKilled = 0;
         starCollected = 0;
+        diamondCollected= 0;
         levelCompleteUI.SetActive(false);
         Time.timeScale = 1f;
     }
     void Update()
     {
         enemyCount.text = enemiesKilled.ToString() + "/" + totalEnemy.ToString();
+        LevelResult.enemyText.text = enemyCount.text;
         healthCount.text = PlayerStats.health.ToString();
+        LevelResult.healthText.text = healthCount.text;
         starCount.text = starCollected.ToString() + "/" + totalStar.ToString();
+        LevelResult.starText.text = starCollected.ToString();
+        diamondCount.text=diamondCollected.ToString()+"/"+totalDiamond.ToString();
+        LevelResult.diamondText.text = diamondCount.text;
         if (enemiesKilled == totalEnemy)
         {
             levelCompleteUI.SetActive(true);
-            Time.timeScale = 0f;
         }
         else
         {
