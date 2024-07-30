3ebe9f8e5db2b6a3f93346836684a224e0f4a105
Muhammad Hadi Naeem <hadinaeem365@gmail.com>
2024-07-30 21:16:03 +0500

    Create LevelResult.cs

---
+++ Assets/Scripts/LevelResult.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelResult : MonoBehaviour
+{
+    public static Text enemyText;
+    public static Text healthText;
+    public static Text starText;
+    public static Text diamondText;
+}
