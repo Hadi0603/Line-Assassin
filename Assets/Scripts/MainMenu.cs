4cd885cf0da2cf0a4356bc5b8984f662bb4bb7bf
Muhammad Hadi Naeem <hadinaeem365@gmail.com>
2024-07-31 18:47:30 +0500

    Update MainMenu.cs

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,10 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
-    
+    [SerializeField] Text starCount;
+    [SerializeField] Text diamondCount;
+    public static int star;
+    public static int diamond;
+    private void Update()
+    {
+        starCount.text = star.ToString();
+        diamondCount.text = diamond.ToString();
+    }
     public void Play()
     {
         SceneManager.LoadScene("Level1");
