cd95280f71d43ad03d514e7b4c81df033bc50886
Muhammad Hadi Naeem <hadinaeem365@gmail.com>
2024-07-31 17:24:03 +0500

    Create GameOverUI.cs

---
+++ Assets/Scripts/GameOverUI.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOverUI : MonoBehaviour
+{
+    private LevelStats stats;
+    [SerializeField] Text enemyText;
+    [SerializeField] Text healthText;
+    [SerializeField] Text starText;
+    [SerializeField] Text diamondText;
+    private void Update()
+    {
+        stats = GetComponent<LevelStats>();
+        enemyText.text = stats.enemyCount.text;
+        healthText.text = stats.healthCount.text;
+        starText.text = stats.starCount.text;
+        diamondText.text = stats.diamondCount.text;
+    }
+}
