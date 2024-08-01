91c9fce2c921cd02d5f01ef3452255c83b1fcbe4
Muhammad Hadi Naeem <hadinaeem365@gmail.com>
2024-08-01 17:10:28 +0500

    Update Wall.cs

--- Assets/Scripts/Wall.cs
+++ Assets/Scripts/Wall.cs
@@ -6,18 +6,21 @@ public class Wall : MonoBehaviour
 {
     private Vector3 startPosition;
     private Quaternion startRotation;
-
-    // Start is called before the first frame update
     void Start()
     {
         startPosition = transform.position;
         startRotation = transform.rotation;
     }
-
-    // Update is called once per frame
     void Update()
     {
         transform.position = startPosition;
         transform.rotation = startRotation;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Bullet"))
+        {
+            Destroy(other.gameObject);
+        }
     }
 }
