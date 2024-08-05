3f4ae51dafba0c36ebedf72790c205db66cc160e
Muhammad Hadi Naeem <hadinaeem365@gmail.com>
2024-08-05 09:05:12 +0500

    Update MainMenu.cs

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -10,6 +10,11 @@ public class MainMenu : MonoBehaviour
     [SerializeField] Text diamondCount;
     public static int star;
     public static int diamond;
+    private void Start()
+    {
+        star = PlayerPrefs.GetInt("star", 0);
+        diamond = PlayerPrefs.GetInt("diamond", 0);
+    }
     private void Update()
     {
         starCount.text = star.ToString();
@@ -17,11 +22,18 @@ public class MainMenu : MonoBehaviour
     }
     public void Play()
     {
-        SceneManager.LoadScene("Level1");
+        SceneManager.LoadScene("LevelSelect");
     }
 
     public void Quit()
     {
+        SaveProgress();
         Application.Quit();
+    }
+    public void SaveProgress()
+    {
+        PlayerPrefs.SetInt("star", star);
+        PlayerPrefs.SetInt("diamond", diamond);
+        PlayerPrefs.Save();
     }
 }
