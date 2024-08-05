2f17c58d4cf5b861e993cc85c66e010dc1ba9754
Muhammad Hadi Naeem <hadinaeem365@gmail.com>
2024-08-05 18:54:06 +0500

    Update LevelSelect.cs

--- Assets/Scripts/LevelSelect.cs
+++ Assets/Scripts/LevelSelect.cs
@@ -7,22 +7,23 @@ using UnityEngine.UI;
 public class LevelSelect : MonoBehaviour
 {
     public Button[] levelButtons;
-    public Color[] enabledColour;
-    public Color disabledColour;
+    public Color[] enabledTextColour;
+    public Color disabledTextColour;
     private void Start()
     {
-        int levelReached = PlayerPrefs.GetInt("levelReached", 1);
+        int levelReached = PlayerPrefs.GetInt("levelReached", 2);
         for (int i = 0; i < levelButtons.Length; i++)
         {
-            if (i + 1 > levelReached)
+            Text buttonText = levelButtons[i].GetComponentInChildren<Text>();
+            if (i + 2 > levelReached)
             {
                 levelButtons[i].interactable = false;
-                levelButtons[i].image.color = disabledColour;
+                buttonText.color = disabledTextColour;
             }
             else
             {
                 levelButtons[i].interactable = true;
-                levelButtons[i].image.color = enabledColour[i];
+                buttonText.color = enabledTextColour[i];
             }
         }
     }
