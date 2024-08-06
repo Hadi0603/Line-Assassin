75dc973a6d42bbcc18549e1a9b585d381d3d9ff6
Muhammad Hadi Naeem <hadinaeem365@gmail.com>
2024-08-06 16:24:30 +0500

    Update MainMenu.cs

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -6,11 +6,19 @@ using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] GameObject tutorial;
     public void Play()
     {
         SceneManager.LoadScene("LevelSelect");
     }
-
+    public void Tutorial()
+    {
+        tutorial.SetActive(true);
+    }
+    public void cancel()
+    {
+        tutorial.SetActive(false);
+    }
     public void Quit()
     {
         SaveProgress();
